2a6f5f82876acfda625000a4ffff4d45ab8e2d8d
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-02-12 17:49:46 -0300

    Add app and finished

--- src/exercicioSet/App.java
+++ src/exercicioSet/App.java
@@ -11,6 +11,38 @@ public class App {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		Set<Course> course = new HashSet<>();
 		
+		System.out.print("How many students for course A? ");
+		int n = sc.nextInt();
+		for(int i = 1; i <= n; i++) {
+			int aluno = sc.nextInt();
+			course.add(new Course(aluno));
+		}
+		
+		System.out.println("");
+		System.out.print("How many students for course B? ");
+			n = sc.nextInt();
+		for(int i = 1; i <= n; i++) {
+			int aluno = sc.nextInt();
+			course.add(new Course(aluno));
+		}
+		
+		System.out.println("");
+		System.out.print("How many students for course C? ");
+			n = sc.nextInt();
+		for(int i = 1; i <= n; i++) {
+			int aluno = sc.nextInt();
+			course.add(new Course(aluno));
+		}
+		
+		System.out.println("");
+		System.out.println("Total student: " + course.size());
+		
+		sc.close();
+	}
 
 }
